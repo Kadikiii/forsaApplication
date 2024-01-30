6f4037813fc5e4e33682cdd4c1555e416cb61af6
Abdullah <abdallahaljehani926@gmail.com>
2024-01-30 16:44:12 +0200

    initializingProject

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import buttonComponentVue from './components/buttonComponent.vue'
+</script>
+
+<template>
+  <div>
+    <buttonComponentVue />
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
